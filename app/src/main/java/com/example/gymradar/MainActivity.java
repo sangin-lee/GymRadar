a6e71f2888c496a30086ce5188533525427c87cb
sangin.lee <sangin.lee.life@gmail.com>
2021-12-04 17:54:28 +0900

    Tested Kakao Map API

--- app/src/main/java/com/example/gymradar/MainActivity.java
+++ app/src/main/java/com/example/gymradar/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.gymradar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_map);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
 }
